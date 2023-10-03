a4bb0a75d771701beb6b1894327508fc393e25cd
Coaditya7 <coaditya74@gmail.com>
2023-10-03 10:08:37 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNotebyHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotebyHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+module.exports = routes;
